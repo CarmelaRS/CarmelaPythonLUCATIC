3efc0f6e5de4bef9774be5bdab22ec598bb8c85e
Carmela Regife <c.regife.sanchez@gmail.com>
2024-12-10 10:48:49 +0100

    Probamos la identacion en python

---
+++ CODIGO/ejercicio1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+""" probamos identacion en Python"""
+
+A = 1
+if A == 1:
+    print("A es igual a 1")
+    print("Test A es 1")
+else:
+    print("A no es igual a 1")
+    print("Test A no es 1")
+
+# Añadimos un comentario que siempre se imprimira
+print("Esto siempre se imprimira")
