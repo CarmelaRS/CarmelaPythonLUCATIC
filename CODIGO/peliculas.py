22a394f29cc8c8c1c89dafc7ba29742bc9401754
Carmela Regife <c.regife.sanchez@gmail.com>
2024-12-10 17:20:51 +0100

    peliculas

--- CODIGO/peliculas.py
+++ CODIGO/peliculas.py
@@ -28,13 +28,30 @@ def listar_peliculas(peliculas):
 
 # Funcion para buscar una pelicula
 
+def buscar_pelicula(peliculas):
+    # Si no hay peliculas, muestra un mensaje
+    if not peliculas:
+        print("\nNo hay películas agregadas.")
+        return  # Salimos de la función si no hay peliculas
+    
+    print("\nBuscar película:")
+    nombre = input("Nombre de la película: ")
+    resultado = [pelicula for pelicula in peliculas if pelicula.lower() == nombre.lower()]
+
+    if not resultado:
+        print(f"\nNo se encontró la película '{nombre}'.")
+    else:
+        print(f"\nSe encontró la película '{nombre}':")
+        for i, pelicula in enumerate(resultado, start=1):
+            print(f"{i}. {pelicula}")
+
 # Funcion para añadir una pelicula
 def agregar_pelicula(peliculas): # Peliculas es una lista de peliculas que se crea en el main
     print("\nAñadir película")
-    pelicula = input("Nombre de la película: ")
+    pelicula = input("Nombre de la película: ").strip() # Quitamos los espacios en blanco al principio y al final de la cadena
     peliculas.append(pelicula)
-    print("\nPelícula ' {pelicula} ' añadida correctamente.")
-
+    # Mostramos la pelicula añadida
+    print(f"Película'{pelicula}'añadida correctamente.")
 
 # Funcion para eliminar una pelicula
 
@@ -56,4 +73,29 @@ def eliminar_pelicula(peliculas):
         print("\nDebe ingresar un número valido.")
 
 # Programa principal
+def main():
+    peliculas = []  # Lista para almacenar las peliculas
+    
+    while True: 
+        opcion = menu_principal()
+        
+        if opcion == "1":
+            listar_peliculas(peliculas)
+        elif opcion == "2":
+            buscar_pelicula(peliculas)
+        elif opcion == "3":
+            agregar_pelicula(peliculas)
+        elif opcion == "4":
+            eliminar_pelicula(peliculas)
+        elif opcion == "5":
+            print("\nSaliendo del programa...")
+            break
+        else:
+            print("\nOpción no válida, por favor intentalo de nuevo.")
+
+# Ejecutamos el programa
+
+if __name__ == "__main__":
+    main()
+
 
