5e20e115aff6d32ed18e8fd9d4815d554ce54f7e
Carmela Regife <c.regife.sanchez@gmail.com>
2024-12-11 20:16:43 +0100

    continuacion de casos de uso

--- CODIGO/ejercicio3.py
+++ CODIGO/ejercicio3.py
@@ -27,3 +27,71 @@ print("Ordenar eventos: ", eventos)
 print("===================================================================")
 
 print("CASOS DE USO DE TUPLAS")
+# Creamos un conjunto de valores que no pueden ser modificados 
+direccion = (["Madrid", "28001", "España"], ["Paris", "75001", "Francia"], ["London", "EC1A 1BB", "Reino Unido"])
+
+# damos variable a cada valor
+ciudad = direccion[0][0]
+cp = direccion[0][1]
+pais = direccion[0][2]
+
+# Otra forma de acceder a los valores
+ciudad2, cp2, pais2 = direccion[1]
+
+# Imprimimos               
+print("Imprimir valores de la direccion española:")
+print("Ciudad: ", ciudad)
+print("CP: ", cp)
+print("Pais: ", pais)
+
+print("Imprimir valores de la direccion francesa:")
+print("Ciudad: ", ciudad2)
+print("CP: ", cp2)
+print("Pais: ", pais2)
+
+# Cambiamos el cp de madrid, para ello: lo pasamos a una lista 
+listaciudades = list(direccion)
+
+# Modificamos la lista 
+listaciudades[0][1] = "28050"
+
+# Volvemos a pasarlo a tupla 
+direcciones = tuple(listaciudades)
+
+#Imprime la tupla modificada 
+print("Tupla modificada: ", direcciones)
+print("===================================================================")
+
+print("CASOS DE USO CONJUNTOS")
+
+# Creamos dos conjuntos distintos de prendas de ropa de mujer y de hombre 
+mujer = {"falda", "pantalones", "tacones", "camiseta"}
+hombre = {"camiseta", "corbata", "pantalones"}
+
+# Comprobamos que los conjuntos tiene prendas especificas 
+print("El armario de la MUJER tiene tacones? ", "tacones" in mujer)
+print("El armario de la HOMBRE tiene tacones? ", "tacones" in hombre)
+
+# Añadimos reloj al armario del hombre 
+hombre.add("Reloj")
+print("Armario del hombre: ", hombre)
+
+# Unimos los armarios
+armarios = mujer | hombre
+print("Union de armarios: ", armarios)
+
+# Añadimos las prendas que esten los dos
+armarios2 = mujer & hombre
+print("Prendas en ambos armarios: ", armarios2)
+
+# DIFERENCIA DE CONJUNTOS
+diffmujer = mujer - hombre
+print("Las prendas que no están en hombre son: ", diffmujer)
+
+diffhombre = hombre - mujer
+print("Las prendas que no están en mujer son: ", diffhombre)
+
+# DIFERENCIA SIMÉTRICA DE CONJUNTOS
+diffarmariosimetrica = mujer ^ hombre
+print("Las prendas que están en algún armario pero no en ambos son: ", diffarmariosimetrica)
+
